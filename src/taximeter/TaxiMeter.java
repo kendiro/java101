8087c85c00e9b371d5c5b44aa71b47e864812054
Kenan Asker <kasker@ebay.com>
2022-01-22 15:53:58 +0300

    Add taximeter

---
+++ src/taximeter/TaxiMeter.java
@@ -0,0 +1,21 @@
+package taximeter;
+
+import java.util.Scanner;
+
+public class TaxiMeter {
+
+    public static void main(String[] args) {
+        int km;
+        double perKm = 2.20, total, startPrice = 10;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Mesafeyi Km cinsingen giriniz: ");
+        km = input.nextInt();
+
+        total = (km * perKm);
+        total += startPrice;
+
+        total = total < 20 ? 20 : total;
+        System.out.println("Toplam Tutar: " + total);
+
+    }
+}
